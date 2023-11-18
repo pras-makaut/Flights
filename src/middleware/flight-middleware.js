f4278c867b561b72c1e218b24adabdffb57e9be5
Ranjan <prasranjan@deloitte.com>
2023-11-18 10:47:54 +0530

    added flight seat update api

--- src/middleware/flight-middleware.js
+++ src/middleware/flight-middleware.js
@@ -44,7 +44,17 @@ function validateCreateRequest(req,res,next){
     next();
 }
 
+function validateUpdateSeatsRequest(req,res,next){
+    if(!req.body.seats){
+        ErrorResponse.message = 'Something went wrong while updating the seats in flight';
+        ErrorResponse.error = new AppError(['seats not found in the incoming request in the correct form'],StatusCodes.BAD_REQUEST);
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse)
+    }
+    next();
+}
+
 
 module.exports = {
     validateCreateRequest,
+    validateUpdateSeatsRequest
 }
